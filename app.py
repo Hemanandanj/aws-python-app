3ba8a4bd0d426baf3beda7bfab6599404e157caf
hemanandan <128038793+Hemanandanj@users.noreply.github.com>
2026-03-18 09:26:05 +0530

    appv1.0

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+from flask import Flask, render_template, request
+import pymysql
 
+app - Flask(__name__)
+
+db_config = {
+    'host': '172.31.19.240',
+    'user': 'app_user',
+    'password': 'HEma12#$',
+    'database': 'app_db',
+
+
+}
