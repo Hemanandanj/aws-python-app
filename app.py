bd97136c1c9c74df5ce264c823b547c028aec8e3
hemanandan <128038793+Hemanandanj@users.noreply.github.com>
2026-03-18 09:41:24 +0530

    appv1.1

--- app.py
+++ app.py
@@ -11,3 +11,26 @@ db_config = {
 
 
 }
+
+@app.route('/')
+def index():
+    return render_template("index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form.get('name')
+    age = request.form.get('age')
+
+    try:
+        connection = pymysql.connect(**db_config)
+        with connection.cursor() ass cursor:
+            sql = "INSERT INTO users (name, age) VALUES (%s, %s)"
+            curson.execute(sql, (name,age))
+        connection.commit()
+        connection.close()
+        return f"<h1>Success!</h1><p>Registered {name}, age {age}.</p><a href='/'>Go Back</a>"
+    except Exception as e:
+        return f"<h1>Error</h1><p>{str(e)</p>}"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000)
